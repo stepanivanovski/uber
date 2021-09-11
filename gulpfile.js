18782f0ba24c583428588f4d62987f0c9a253623
Stepan <usb-1999@mail.ru>
2021-09-11 18:54:17 +0600

    Create dist folder

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+const gulp        = require('gulp');
+const browserSync = require('browser-sync');
+const sass        = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require("gulp-rename");
+
+gulp.task('server', function() {
+
+    browserSync({
+        server: {
+            baseDir: "dist"
+        }
+    });
+
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('styles', function() {
+    return gulp.src("src/sass/**/*.+(scss|sass)")
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer())
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("dist/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function() {
+    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+    gulp.watch("src/*.html").on('change', gulp.parallel('html'));
+})
+
+gulp.task('html', function () {
+    return gulp.src("src/*.html")
+        .pipe(gulp.dest("dist/"));
+});
+
+
+gulp.task('images', function () {
+    return gulp.src("src/img/**/*")
+        .pipe(gulp.dest("dist/img"));
+});
+
+gulp.task('scripts', function () {
+    return gulp.src("src/js/*.js")
+        .pipe(gulp.dest("dist/js"));
+});
+
+gulp.task('icons', function () {
+    return gulp.src("src/icons/**/*")
+        .pipe(gulp.dest("dist/icons"));
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'html', 'images', 'icons', 'scripts'));
